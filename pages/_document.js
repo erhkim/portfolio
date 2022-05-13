5aeca6c8a5432980efaf7a1505eb702f9b59d76e
Eric Kim <erhkim@protonmail.com>
2022-05-12 17:23:53 -0700

    Updated to beta nextra

--- pages/_document.js
+++ pages/_document.js
@@ -1,42 +1,32 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document'
 
-class MyDocument extends Document {
-  static async getInitialProps(ctx) {
-    const initialProps = await Document.getInitialProps(ctx)
-    return { ...initialProps }
+export default function Document() {
+  const meta = {
+    title: 'Next.js Blog Starter Kit',
+    description: 'Clone and deploy your own Next.js portfolio in minutes.',
+    image: 'https://assets.vercel.com/image/upload/q_auto/front/vercel/dps.png'
   }
 
-  render() {
-    const meta = {
-      title: 'Eric Kim',
-      description: 'Clone and deploy your own Next.js portfolio in minutes.',
-      image:
-        'https://assets.vercel.com/image/upload/q_auto/front/vercel/dps.png'
-    }
-
-    return (
-      <Html lang="en">
-        <Head>
-          <meta name="robots" content="follow, index" />
-          <meta name="description" content={meta.description} />
-          <meta property="og:site_name" content={meta.title} />
-          <meta property="og:description" content={meta.description} />
-          <meta property="og:title" content={meta.title} />
-          <meta property="og:image" content={meta.image} />
-          <meta name="twitter:card" content="summary_large_image" />
-          <meta name="twitter:site" content="@yourname" />
-          <meta name="twitter:title" content={meta.title} />
-          <meta name="twitter:description" content={meta.description} />
-          <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🪨</text></svg>"></link>
-          {/* <meta name="twitter:image" content={meta.image} /> */}
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    )
-  }
+  return (
+    <Html lang="en">
+      <Head>
+        <meta name="robots" content="follow, index" />
+        <meta name="description" content={meta.description} />
+        <meta property="og:site_name" content={meta.title} />
+        <meta property="og:description" content={meta.description} />
+        <meta property="og:title" content={meta.title} />
+        <meta property="og:image" content={meta.image} />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@yourname" />
+        <meta name="twitter:title" content={meta.title} />
+        <meta name="twitter:description" content={meta.description} />
+        <meta name="twitter:image" content={meta.image} />
+        <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🪨</text></svg>"></link>
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
 }
-
-export default MyDocument
--- theme.config.js
+++ theme.config.js
@@ -17,26 +17,15 @@ export default {
       `}</style>
     </footer>
   ),
-  head: ({ title, meta }) => (
-    <>
-      {meta.description && <meta name="description" content={meta.description} />}
-      {meta.tag && <meta name="keywords" content={meta.tag} />}
-      {meta.author && <meta name="author" content={meta.author} />}
-    </>
-  ),
-  readMore: 'Read More →',
-  titleSuffix: null,
-  postFooter: null,
-  cusdis: {
-    appId: 'your_app_id',
-    host: 'your_host(optional)',
-    lang: 'your_lang'
-  },
-  darkMode: false,
-  navs: [
-    {
-      url: 'https://github.com/shuding/nextra',
-      name: 'Nextra'
-    }
-  ]
+  // head: ({ title, meta }) => (
+  //   <>
+  //     {meta.description && <meta name="description" content={meta.description} />}
+  //     {meta.tag && <meta name="keywords" content={meta.tag} />}
+  //     {meta.author && <meta name="author" content={meta.author} />}
+  //   </>
+  // ),
+  // readMore: 'Read More →',
+  // titleSuffix: null,
+  // postFooter: null,
+  // darkMode: false
 }
